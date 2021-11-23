0cd3c5b87e6a01ac276a05642d21bf2cafb8e80f
Jonathan <jonathan.steele232@gmail.com>
2021-11-23 09:10:10 -0700

    Timeout feature complete
    Keeps printing to the console if no event comes in
    (for each of the three states)
    
            modified:   src/dog_fsm.erl

--- dog_fsm/src/dog_fsm.erl
+++ dog_fsm/src/dog_fsm.erl
@@ -34,37 +34,72 @@ code_change(_Vsn, State, Data, _Extra) ->
 init([]) ->
     %% Set the initial state + data.  Data is used only as a counter.
     State = bark, Data = 0,
-    {ok,State,Data}.
+    {
+      %% Start in bark state
+      ok,State,Data, 
+      %% With timeout events enabled from the start
+      [{state_timeout, 2000, bark_again}] 
+    }.
 callback_mode() -> state_functions.
 
 %%% state callback(s)
 
+bark(state_timeout, bark_again, Data) ->
+    io:format("Dog says: BARK! BARK!~n"),
+    {
+      next_state,bark,Data,
+      [{state_timeout, 2000, bark_again}]
+    };
 bark({call,From}, pet, Data) ->
     io:format("Dog says: BARK! BARK!~n"),
     %% Go to 'wag_tail' and reply
     %% that the resulting status is 'wag_tail'
-    {next_state,wag_tail,Data,[{reply,From,wag_tail}]};
-    %% TODO figure out timeouts for looping through barking
+    %% Or if enough time passes without events, keep wagging :) 
+    {
+      next_state,wag_tail,Data,
+      [{reply,From,wag_tail}, {state_timeout, 2000, wag_again}]
+    };
 bark(EventType, EventContent, Data) ->
     io:format("Dog is confused~n"),
+    %% TODO send no_reply or something IDK -- would that make it nicer for clients?
     handle_event(EventType, EventContent, Data).
 
+
+
+wag_tail(state_timeout, wag_again, Data) ->
+    io:format("Dog wags its tail~n"),
+    {
+      next_state,wag_tail,Data,
+      [{state_timeout, 2000, wag_again}]
+    };
 wag_tail({call,From}, pet, Data) ->
     io:format("Dog wags its tail~n"),
     %% Go to 'sit' and reply
     %% that the resulting status is 'sit'
-    {next_state,sit,Data,[{reply,From,sit}]};
-    %% TODO figure out timeouts for going back to barking
+    {
+      next_state,sit,Data,
+      [{reply,From,sit},{state_timeout,2000,sit_again}]
+    };
 wag_tail(EventType, EventContent, Data) ->
     io:format("Dog is confused~n"),
     handle_event(EventType, EventContent, Data).
 
+
+
+sit(state_timeout, sit_again, Data) ->
+    io:format("Dog is sitting. Gooooood boy!~n"),
+    {
+      next_state,sit,Data,
+      [{state_timeout, 2000, sit_again}]
+    };
 sit({call,From}, squirrel, Data) ->
     io:format("Dog is sitting. Gooooood boy!~n"),
     %% Go to 'bark' and reply
     %% that the resulting status is 'bark'
-    {next_state,bark,Data,[{reply,From,bark}]};
-    %% TODO figure out timeouts for going back to sitting
+    {
+      next_state,bark,Data,
+      [{reply,From,bark},{state_timeout,2000,bark_again}]
+    };
 sit(EventType, EventContent, Data) ->
     io:format("Dog is confused~n"),
     handle_event(EventType, EventContent, Data).
@@ -79,4 +114,11 @@ handle_event({call,From}, get_count, Data) ->
 handle_event(_, _, Data) ->
     %% Ignore all other events
     {keep_state,Data}.
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%
+% Internal functions
+%%%%%%%%%%%%%%%%%%%%%
 
